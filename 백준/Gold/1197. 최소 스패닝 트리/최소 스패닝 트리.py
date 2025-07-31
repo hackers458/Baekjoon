a9eee928293be221f8c9c0309c965d3cfebcabf8
hackers458 <hackers458@naver.com>
2025-07-31 16:44:02 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 496 ms, Memory: 54320 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,43 @@
+import sys
+sys.setrecursionlimit(1000000000)
+from queue import PriorityQueue
+input = sys.stdin.readline
+N,M = map(int,input().split())
+pq = PriorityQueue()
+parent = [0] * (N+1)
+
+for i in range(N+1):
+    parent[i] = i
+
+for i in range(M):
+    s,e,w = map(int,input().split())
+    pq.put((w,s,e)) # 가중치가 우선순위이므로 맨 앞에 둠
+
+
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a]) # 경로 압축,
+        return parent[a]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    if a!=b:
+        parent[b] = a
+
+
+useEdge = 0
+result = 0
+
+while useEdge < N-1:
+    w,s,e = pq.get()
+    if find(s) != find(e):
+        union(s,e)
+        result +=w
+        useEdge +=1
+
+
+
+print(result)
