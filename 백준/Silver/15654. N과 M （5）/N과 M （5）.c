8ed192047d4719a234752d78da8e9bec5ef0c501
hackers458 <hackers458@naver.com>
2025-07-31 16:40:00 +0900

    [Silver III] Title: N과 M (5), Time: 32 ms, Memory: 1120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.c
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int array[9];
+int visit[9];
+int stack[1000];
+int N, M;
+
+
+int compare(const void* a, const void* b)
+{
+	if (*(int*)a > *(int*)b) return 1;
+	else if (*(int*)a < *(int*)b) return -1;
+	else return 0;
+}
+
+
+void backtracking(int depth)
+{
+	if (depth == M) {
+		for (int i = 0; i <M; i++) {
+			printf("%d ", stack[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	
+
+	for (int i = 0; i < N; i++) {
+		if (visit[i] == 0) {
+			visit[i] = 1;
+			stack[depth] = array[i];
+			backtracking(depth + 1);
+			visit[i] = 0;
+		}
+	}
+	return;
+}
+
+int main(void){
+
+
+	scanf("%d %d", &N, &M);// 범위와 수열의 길이
+	int a;
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &array[i]);
+	}
+
+	qsort(array, N, sizeof(int), compare);
+	backtracking(0);
+
+	return 0;
+}
